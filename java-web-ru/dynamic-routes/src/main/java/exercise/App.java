20562ee5628fc3ed6502b4cae1356c1d1a5fd9e6
@hexlet/cli <support@hexlet.io>
2025-01-23 14:03:55 +0500

    submit java-web-ru/dynamic-routes

---
+++ java-web-ru/dynamic-routes/src/main/java/exercise/App.java
@@ -0,0 +1,51 @@
+package exercise;
+
+import io.javalin.Javalin;
+import io.javalin.http.NotFoundResponse;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+// BEGIN
+
+// END
+
+public final class App {
+
+    private static final List<Map<String, String>> COMPANIES = Data.getCompanies();
+
+    public static Javalin getApp() {
+
+        var app = Javalin.create(config -> {
+            config.bundledPlugins.enableDevLogging();
+        });
+
+        // BEGIN
+        app.get("/companies/{id}", ctx -> {
+            var id = ctx.pathParamAsClass("id", Integer.class).get();
+            if (id > COMPANIES.size() || id < 0) {
+                throw new NotFoundResponse("Company not found");
+            } else ctx.json(COMPANIES.get(id)); ;
+
+
+        });
+        // END
+
+        app.get("/companies", ctx -> {
+            ctx.json(COMPANIES);
+        });
+
+        app.get("/", ctx -> {
+            ctx.result("open something like (you can change id): /companies/5");
+        });
+
+        return app;
+
+    }
+
+    public static void main(String[] args) {
+        Javalin app = getApp();
+        app.start(7070);
+    }
+}
