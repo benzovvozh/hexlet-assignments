93a21c4745275f00bebcfe4defa835be1805a330
@hexlet/cli <support@hexlet.io>
2025-02-20 19:05:29 +0500

    submit java-web-ru/middlewares

--- java-web-ru/middlewares/src/main/java/exercise/App.java
+++ java-web-ru/middlewares/src/main/java/exercise/App.java
@@ -14,18 +14,6 @@ import java.security.MessageDigest;
 
 public final class App {
 
-    private static String bytesToHex(byte[] hash) {
-        StringBuilder hexString = new StringBuilder(2 * hash.length);
-        for (int i = 0; i < hash.length; i++) {
-            String hex = Integer.toHexString(0xff & hash[i]);
-            if (hex.length() == 1) {
-                hexString.append('0');
-            }
-            hexString.append(hex);
-        }
-        return hexString.toString();
-    }
-
     public static Javalin getApp() {
 
         var app = Javalin.create(config -> {
@@ -33,11 +21,7 @@ public final class App {
             config.fileRenderer(new JavalinJte());
         });
 
-        app.before(ctx -> {
-            MessageDigest digest = MessageDigest.getInstance("SHA-256");
-            byte[] encodedhash = digest.digest();
-            ctx.header("X-Response-Digest", bytesToHex(encodedhash));
-        });
+
 
         app.get(NamedRoutes.rootPath(), RootController::index);
 
@@ -51,7 +35,11 @@ public final class App {
         app.post(NamedRoutes.postPath("{id}"), PostsController::update);
 
         // BEGIN
-
+        app.after(ctx -> {
+            var prevBody = ctx.result();
+            var digest = DigestUtils.sha256Hex(prevBody);
+            ctx.header("X-Response-Digest", digest);
+        });
         // END
 
         return app;
