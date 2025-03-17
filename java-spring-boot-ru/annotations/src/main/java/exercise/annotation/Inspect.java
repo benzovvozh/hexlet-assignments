5e1b69e9b46aa60c31636a57312651076a2bf07c
@hexlet/cli <support@hexlet.io>
2025-03-17 16:51:26 +0500

    submit java-spring-boot-ru/annotations

---
+++ java-spring-boot-ru/annotations/src/main/java/exercise/annotation/Inspect.java
@@ -0,0 +1,11 @@
+package exercise.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+// BEGIN
+@Target(ElementType.METHOD)
+public @interface Inspect{
+
+}
+// END
